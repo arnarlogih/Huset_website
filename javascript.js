4d6e57c900a5c1717fd73e3d63f5b2aec123b69a
Arnar Logi <arnarlogi95@gmail.com>
2019-04-10 20:27:10 +0200

    burgermenu

--- javascript.js
+++ javascript.js
@@ -1,4 +1,15 @@
 const headerClass = document.querySelector(".header");
+var overlay = document.getElementById("overlay");
+var closeMenu = document.getElementById("close-menu");
+
+document.getElementById("open-menu").addEventListener("click", function() {
+    overlay.classList.add("show-menu");
+});
+
+document.getElementById("close-menu").addEventListener("click", function() {
+    overlay.classList.remove("show-menu");
+});
+
 
 
 let scrollPosition = window.scrollY;
