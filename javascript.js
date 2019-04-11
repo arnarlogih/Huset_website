79897e1a322d41b551ca6c0618dd2ad908f03825
Sigurdarson <gunnar@sigurdarson.is>
2019-04-11 09:54:42 +0200

    Mobile menu & and small fixes. like search bar

--- javascript.js
+++ javascript.js
@@ -1,16 +1,7 @@
 const headerClass = document.querySelector(".header");
-var overlay = document.getElementById("overlay");
-var closeMenu = document.getElementById("close-menu");
-
-document.getElementById("open-menu").addEventListener("click", function() {
-    overlay.classList.add("show-menu");
-});
-
-document.getElementById("close-menu").addEventListener("click", function() {
-    overlay.classList.remove("show-menu");
-});
-
-
+let overlay = document.querySelector(".overlay");
+let openBtn = document.querySelector(".open-btn");
+let closeBtn = document.querySelector(".close-btn");
 
 let scrollPosition = window.scrollY;
 
@@ -24,3 +15,26 @@ window.addEventListener("scroll", function() {
         headerClass.classList.remove("scrolled");
     }
 });
+
+openBtn.addEventListener("click", () => {
+    overlay.classList.remove("hide");
+    headerClass.classList.add("headerMobileOpen");
+    openBtn.classList.add("close-btn");
+    setTimeout(function(){ 
+        openBtn.classList.add("hide");
+        openBtn.classList.remove("close-btn");
+        closeBtn.classList.remove("hide");
+    }, 1000);
+});
+
+closeBtn.addEventListener("click", () => {
+    overlay.classList.add("hide");
+    headerClass.classList.remove("headerMobileOpen");
+    closeBtn.classList.remove("close-btn");
+    setTimeout(function(){ 
+        closeBtn.classList.add("hide");
+        closeBtn.classList.add("close-btn");
+        openBtn.classList.remove("hide");
+    }, 1000);
+});
+
